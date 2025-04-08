4a2522773dc9a3148e9abab1d37dd18a5417208b
tifufu <dematasjoshua@hotmail.com>
2025-04-08 13:10:42 +0200

    refactor: use mutations for button triggers feat: buttons may now invalidate other url

--- components/schema/button.tsx
+++ components/schema/button.tsx
@@ -8,21 +8,46 @@ import {
 import { Assert } from '@/lib/assert'
 import { useThemeColor } from '../Themed'
 import { TextSourceComponent } from './hooks/useTextSource'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 type ButtonDefinition = ComponentDefinition<{
     target: string
     text: string | HTTPTextSource | StringTextSource
+    invalidatesTargets?: string[]
 }>
 function ButtonComponent(def: ButtonDefinition) {
     Assert(def.type === 'button', "expected type 'button'")
     const textColor = useThemeColor({}, 'primaryContent')
     const bgColor = useThemeColor({}, 'primary')
     const shadowColor = useThemeColor({}, 'neutral')
+    const qclient = useQueryClient()
+    const q = useMutation({
+        mutationKey: [def.target],
+        mutationFn: async function () {
+            const res = await fetch(def.target, { method: 'POST' })
+            if (res.status > 299) {
+                throw new Error(await res.text())
+            }
+        },
+        onSuccess: function () {
+            if (!def.invalidatesTargets) return
+            Assert(
+                Array.isArray(def.invalidatesTargets),
+                'expected array of string',
+            )
+            def.invalidatesTargets.forEach((t) =>
+                qclient.invalidateQueries({
+                    queryKey: [t],
+                    exact: false,
+                }),
+            )
+        },
+    })
 
-    function handeOnPress() {
+    async function handeOnPress() {
         Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
         console.log('trigger pressed')
-        fetch(def.target)
+        q.mutate()
     }
 
     return (
