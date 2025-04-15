1d88bfdc971eaafda71a26e29bad7e6c4da95ec6
Joshua De Matas <dematasjoshua@hotmail.com>
2025-04-15 21:28:13 +0200

    fix: allow user to define '0' gap in stack feat: change default spacing for stack to 8

--- components/schema/stack.tsx
+++ components/schema/stack.tsx
@@ -37,7 +37,7 @@ function StackComponent(def: StackDefintion) {
         <View
             style={{
                 display: 'flex',
-                gap: def.gap ?? 16,
+                gap: def.gap !== undefined ? def.gap : 8,
                 maxWidth: '100%',
                 flexDirection:
                     def.orientation === 'vertical' ? 'column' : 'row',
